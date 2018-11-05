3c0d1f868ee491bbd5426f487e23eac322b21b27
bagasprasetyap <bagaspp11@gmail.com>
2018-11-05 10:21:31 +0700

    Ini komit ketiga

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // v 1.0
 
 var pangkat = (x, y) => {
-    return x ** y
+    return Math.pow(x,y)
 }
 console.log(pangkat(2,3));
 
 // git rm --cached app.js | delete file yang mau dicommit
+// git add .
 // git commit -m "ini komit pertamaku"
